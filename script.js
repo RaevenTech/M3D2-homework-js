2b31fd787c3e1fcbb34b8c9bb667d8ba3d03b42d
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-04-05 15:55:42 +0100

    not displaying right

---
+++ script.js
@@ -0,0 +1,112 @@
+fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem",
+    options
+)
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response.data[0].duration);
+
+        const divEminem = document.querySelector("eminem");
+
+        for (let i = 0; i < response.data.length; i++) {
+            let newColEminem = document.createElement("div");
+            newColEminem.classList.add("col-sm-3");
+
+            console.log(newColEminem);
+
+            newColEminem.innerHTML = `<div class="card"">
+            <img src="${
+                songs.data[i].album.cover
+            }" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${response.data[i].title}</h5>
+              <p class="card-text">${
+                  "This album has a duration of: " +
+                  response.data[i].duration +
+                  " min"
+              }</p>
+              <a href="${
+                  response.data[i].link
+              }" class="btn btn-primary">LISTEN</a>
+            </div>
+          </div>
+            `;
+            divEminem.appendChild(newColEminem);
+        }
+    });
+
+fetch(
+    " https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica",
+    options
+)
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response.data[0].duration);
+
+        const divMetallica = document.getElementById("metallica");
+
+        for (let i = 0; i < response.data.length; i++) {
+            let newColMetallica = document.createElement("div");
+            newColMetallica.classList.add("col-sm-3");
+
+            console.log(newColMetallica);
+
+            newColMetallica.innerHTML = `<div class="card"">
+                <img src="${
+                    response.data[i].album.cover
+                }" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${response.data[i].title}</h5>
+                  <p class="card-text">${
+                      "This album has a duration of: " +
+                      response.data[i].duration +
+                      " min"
+                  }</p>
+                  <a href="${
+                      response.data[i].link
+                  }" class="btn btn-primary">LISTEN</a>
+                </div>
+              </div>
+                `;
+            divMetallica.appendChild(newColMetallica);
+        }
+    });
+
+fetch(
+    " https://striveschool-api.herokuapp.com/api/deezer/search?q=behemoth",
+    options
+)
+    .then((response) => response.json())
+    .then((responce) => {
+        console.log(responce.data[0].duration);
+
+        let divBehemoth = document.getElementById("behemoth");
+
+        for (let i = 0; i < responce.data.length; i++) {
+            let newColBehemoth = document.createElement("div");
+            newColBehemoth.classList.add("col-sm-3");
+
+            console.log(newColBehemoth);
+
+            newColBehemoth.innerHTML = `<div class="card"">
+                    <img src="${
+                        responce.data[i].album.cover
+                    }" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${responce.data[i].title}</h5>
+                      <p class="card-text">${
+                          "This album has a duration of: " +
+                          responce.data[i].duration +
+                          " min"
+                      }</p>
+                      <a href="${
+                          responce.data[i].link
+                      }" class="btn btn-primary">BUY NOW</a>
+                    </div>
+                  </div>
+                    `;
+            divBehemoth.appendChild(newColBehemoth);
+        }
+    });
+
+/*             .catch(err => console.error(err)); */
